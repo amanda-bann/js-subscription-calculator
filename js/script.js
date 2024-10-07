c0f1ee57738766b4c0f4910770f8eaa95e5900d4
amanda-bann <amanda_klap@protonmail.com>
2024-10-07 11:54:36 -0600

    Call function to update the DOM

--- js/script.js
+++ js/script.js
@@ -7,18 +7,20 @@ var subDuration = 1;
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
   //  console.log(subType);
+  updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
   //  console.log(subDuration);
+  updateSubscriptionDiv ();
 });
 
 var updateSubscriptionDiv = function () {
     var monthlyCost = 5;
     if (subType === "standard") {
         monthlyCost = 7;
-    } else if (subType === "premiume"){
+    } else if (subType === "premium") {
         monthlyCost = 10;
     }
     var total = subDuration * monthlyCost;
